6bff463545d90daba7fef531fdd784c5a0060401
Рустам Астафеев <astafeev0308@gmail.com>
2023-08-18 17:23:23 +0500

    Fixed Celery autodiscover_tasks() can't discover tasks

--- core/celery.py
+++ core/celery.py
@@ -1,6 +1,8 @@
+import os
+
 from celery import Celery
 
-from core.settings import REDIS_BASE_URL, TIME_ZONE
+from core.settings import REDIS_BASE_URL, TIME_ZONE, BASE_DIR
 
 app = Celery('core')
 
@@ -12,4 +14,11 @@ app.conf.task_serializer = 'json'
 app.conf.result_serializer = 'json'
 app.conf.accept_content = ['json']
 
-app.autodiscover_tasks()
+module_names = []
+modules = os.listdir(BASE_DIR)
+for module in modules:
+    if os.path.isdir(os.path.join(BASE_DIR, module)) and \
+            os.path.exists(os.path.join(BASE_DIR, module, 'tasks.py')):
+        module_names.append(f'{module}.tasks')
+
+app.autodiscover_tasks(module_names, force=True)
