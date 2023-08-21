9162ee75564e4bc4fd6b16674890fe777e972161
Рустам Астафеев <astafeev0308@gmail.com>
2023-08-21 17:39:13 +0500

    Added feature to get rates without query

--- bot/inline.py
+++ bot/inline.py
@@ -20,20 +20,17 @@ def sep(s, thou=" ", dec="."):
 
 
 async def inline_query(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Handle the inline query. This is run when you type: @botusername <query>"""
     query = update.inline_query.query
-
     user = await User.from_update(update)
     lang = user.language_code
     activate_locale(lang)
 
-    if not query:  # empty query should not be handled
-        return
+    if not query:
+        query = 1
     try:
         query = float(query)
     except ValueError:
         return
-
     results = []
 
     for curr in SUPPORTED_CURRENCIES:
@@ -52,16 +49,23 @@ async def inline_query(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
             selling = sep('{0:.2f}'.format(exchange_rate.sell_rate))
             buying_converted = sep('{0:.2f}'.format(exchange_rate.buy_rate * query))
             selling_converted = sep('{0:.2f}'.format(exchange_rate.sell_rate * query))
-            text += (f'\n\n---\n\n'
-                     f'🏦 <b>{exchange_rate.source} ({exchange_rate.created_at.strftime("%d.%m.%Y")})</b>\n\n'
-                     f'<b>{query} {curr}</b>\n\n'
-                     f' - {buying_str}: <b>{buying_converted}</b>\n'
-                     f' - {selling_str}: <b>{selling_converted}</b>\n\n'
-                     f'<b>1 {curr}</b>\n\n'
-                     f' - {buying_str}: <b>{buying}</b>\n'
-                     f' - {selling_str}: <b>{selling}</b>\n\n'
-                     f'---\n\n'
-                     f'{cb_rate_str}: <b>{cb_rate}</b>\n\n')
+            text += (
+                f'\n\n---\n\n'
+                f'🏦 <b>{exchange_rate.source} ({exchange_rate.created_at.strftime("%d.%m.%Y")})</b>\n\n'
+            )
+            if query != 1:
+                text += (
+                    f'<b>{query} {curr}</b>\n\n'
+                    f' - {buying_str}: <b>{buying_converted}</b>\n'
+                    f' - {selling_str}: <b>{selling_converted}</b>\n\n'
+                )
+            text += (
+                f'<b>1 {curr}</b>\n\n'
+                f' - {buying_str}: <b>{buying}</b>\n'
+                f' - {selling_str}: <b>{selling}</b>\n\n'
+                f'---\n\n'
+                f'{cb_rate_str}: <b>{cb_rate}</b>\n\n'
+            )
 
         description = (f'{buying_str}: {sep("{0:.2f}".format(first_exchange_rate.buy_rate))} '
                        f'({first_exchange_rate.source})\n'
